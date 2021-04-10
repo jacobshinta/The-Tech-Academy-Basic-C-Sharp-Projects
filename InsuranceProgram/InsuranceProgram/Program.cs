638016a1b2a9d144769c80f36534df49612209de
jacobshinta <jacobshinta@me.com>
2021-04-10 10:17:17 -0400

    add comments

--- InsuranceProgram/InsuranceProgram/Program.cs
+++ InsuranceProgram/InsuranceProgram/Program.cs
@@ -6,16 +6,19 @@ namespace InsuranceProgram
     {
         static void Main()
         {
-            Console.WriteLine("Do you qualify for car insurance?\nWhat is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
-            bool ageResult = age > 15;
-            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\"");
-            bool dui = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("How many speeding tickets do you have?");
-            int tickets = Convert.ToInt32(Console.ReadLine());
-            bool ticketsResult = tickets <= 3;
-            bool status = ageResult && ticketsResult && !dui;
-            Console.WriteLine("Qualified?\n" + status);
+            Console.WriteLine("Do you qualify for car insurance?\nWhat is your age?"); //ask for age
+            int age = Convert.ToInt32(Console.ReadLine()); //store and convert age to int
+            bool ageResult = age > 15; //store bool
+
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\""); //ask for true or false
+            bool dui = Convert.ToBoolean(Console.ReadLine()); //store and convert string to bool
+            
+            Console.WriteLine("How many speeding tickets do you have?"); //ask how many tickets
+            int tickets = Convert.ToInt32(Console.ReadLine()); //store and convert to int
+            bool ticketsResult = tickets <= 3; //store bool for tickets are less than or equal to 3
+            
+            bool status = ageResult && ticketsResult && !dui; //store bool for parameters designated
+            Console.WriteLine("Qualified?\n" + status); //display the stored bool for qualification status
             Console.ReadLine();
             
 
