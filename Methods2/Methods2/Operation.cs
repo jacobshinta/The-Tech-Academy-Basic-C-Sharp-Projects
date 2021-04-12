c6466b7e6780f871a01fb3cf1f391c75e89dd4b5
jacobshinta <jacobshinta@me.com>
2021-04-12 12:24:46 -0400

    add Methods2

---
+++ Methods2/Methods2/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Operation
+    {
+        public void Method(int input1, int input2 = 0)
+        {
+            int result = input1 + input2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation opClass = new Operation();
+            Console.WriteLine("Type an integer to add.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optionally enter a second integer or press enter to continue.");
+            string input2 = Console.ReadLine();
+            if (input2 =="")
+            {
+                opClass.Method(input1);
+            }
+            else
+            {
+                int input2Convert = Convert.ToInt32(input2);
+                opClass.Method(input1, input2Convert);
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
