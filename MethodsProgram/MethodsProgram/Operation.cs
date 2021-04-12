645c62b2b2c07bfae14a4bedc9c29b7745f609d9
jacobshinta <jacobshinta@me.com>
2021-04-12 11:15:58 -0400

    add MethodsProgram

---
+++ MethodsProgram/MethodsProgram/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsProgram
+{
+    public class Operation
+    {
+        public int Add(int userInput) //creating method
+        {
+            userInput = userInput + 10; //adding 10 to the input and restoring the value
+            return userInput;
+            
+        }
+        public int Subtract(int userInput2)
+        {
+            userInput2 = userInput2 - 10;
+            return userInput2;
+
+        }
+        public int Multiply(int userInput3)
+        {
+            userInput3 = userInput3 * 10;
+            return userInput3;
+
+        }
+    }
+        
+
+    
+}
---
+++ MethodsProgram/MethodsProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operations = new Operation(); //create bariable and set it to new method
+
+            Console.WriteLine("Please choose a number to operate on."); //asks for  a number
+            int userInput = Convert.ToInt32(Console.ReadLine()); //converts and stores the number
+            int userInput2 = userInput; // store as a different variable
+            int userInput3 = userInput;
+
+            userInput = operations.Add(userInput); //calls other methods
+            userInput2 = operations.Subtract(userInput2);
+            userInput3 = operations.Multiply(userInput3);
+
+            Console.WriteLine(userInput + " " + userInput2 + " " + userInput3); //displays result
+            Console.ReadLine();
+        }
+
+        
+    }
+}
