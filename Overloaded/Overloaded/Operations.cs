e010603f9c034015110532b22279fb2f28c597d0
jacobshinta <jacobshinta@me.com>
2021-04-12 12:05:27 -0400

    add OverloadProgram

---
+++ Overloaded/Overloaded/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded
+{
+    class Operations
+    {
+        public int Method(int inputInt) //method that takes in the input integer
+        {
+            inputInt = inputInt + 5; //set variable = to operation
+            return inputInt;
+        }
+
+        public double Method(double inputDec) //same as above except with a double
+        {
+            inputDec = inputDec * 5;
+            return inputDec;
+        }
+
+        public string Method(string inputString)
+        {
+            int intString = Convert.ToInt32(inputString); //convert the string to an integer to use and then do the operation and output the result
+            intString = intString / 5;
+            inputString = Convert.ToString(intString);
+            return inputString;
+        }
+    }
+}
---
+++ Overloaded/Overloaded/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations opClass = new Operations(); //create new variable
+
+            int inputInt = 10; //create variables for operations
+            double inputDec = 1.5;
+            string inputString = "10";
+
+            inputInt = opClass.Method(inputInt); //set the variables equal to the result of the method with the inputted value
+            inputDec = opClass.Method(inputDec);
+            inputString = opClass.Method(inputString);
+
+            Console.WriteLine(inputInt + ", " + inputDec + ", " + inputString); //write the result
+            Console.ReadLine();
+        }
+    }
+}
