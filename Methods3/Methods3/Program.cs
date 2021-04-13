5e4ae2f324a70766b388e9e57eafe262c0a496bc
jacobshinta <jacobshinta@me.com>
2021-04-13 07:11:01 -0400

    add methods3

---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestC testC = new TestC(); //instantiate the calss and call the method with these parameters
+            testC.Method(5, 10);
+        }
+    }
+}
---
+++ Methods3/Methods3/TestC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class TestC
+    {
+        public void Method(int input1, int input2)
+        {
+            int var = input1 + 10; //operation and store variable 
+            Console.WriteLine(input2); //display the 2nd parameter
+            Console.ReadLine();
+        }
+    }
+}
