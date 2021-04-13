cfc38bd58ea9122bc77da7aee7b2e434b07d0c9f
jacobshinta <jacobshinta@me.com>
2021-04-13 12:14:13 -0400

    add methods4

---
+++ Methods4/Methods4/Example.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    public class Example
+    {
+        public static void Method(int x, out int output) //create method 
+        {
+            output = x / 2; //divide the variable
+        }
+
+        public static void Method(int x, int y, out int total)
+        {
+            total = x + y;
+        }  
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number and we'll divide it by 2."); //ask for variable
+            int num = Convert.ToInt32(Console.ReadLine()); //convert and store the input
+
+            Example.Method(num, out int x); //call method
+            Console.WriteLine(x); //display result
+
+            Console.WriteLine("Enter another number to be added to the result."); //ask for another variable
+            int y = Convert.ToInt32(Console.ReadLine()); //store and convert
+            Example.Method(x, y, out int total); //call method
+            Console.WriteLine("The total is: " + total); //display result
+            Console.ReadLine();
+        }
+    }
+}
